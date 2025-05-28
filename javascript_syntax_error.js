ff05dfa65553778f7267eb1ca6977414a87d9765
Mahesh Gaire <mgaire34540@ucumberlands.edu>
2025-05-28 09:04:55 -0400

    Create javascript_syntax_error.js
    Javascript syntax error

---
+++ javascript_syntax_error.js
@@ -0,0 +1,11 @@
+// JavaScript: Syntax error example (missing closing brace)
+
+function calculateSum(arr) {
+    let total = 0;
+    for (let num of arr) {
+        total += num;
+    // ❌ MISSING CLOSING BRACE FOR FUNCTION
+
+let numbers = [1, 2, 3, 4, 5];
+let result = calculateSum(numbers);
+console.log("Sum in JavaScript:", result);
