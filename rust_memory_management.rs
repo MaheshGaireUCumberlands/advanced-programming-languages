58451a0e475b53b51bac05533e467d3a742b6192
Mahesh Gaire <mgaire34540@ucumberlands.edu>
2025-05-28 09:44:33 -0400

    Update rust_memory_management.rs

--- rust_memory_management.rs
+++ rust_memory_management.rs
@@ -1,6 +1,6 @@
 fn main() {
     // Allocate a vector on the heap
-    let mut numbers = vec![1, 2, 3, 4, 5];
+    let numbers = vec![1, 2, 3, 4, 5];
     
     // Borrow a slice of the vector
     let slice = &numbers[1..3];
